06bcdf4f954e65c704206129c71ba6286e4dd27e
Andon Mitev <mitevandon94@gmail.com>
2020-05-30 21:05:44 +0300

    Init profile page

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './services/firebase';
 import Authentication from './components/pages/Authentication/Authentication';
 
 import './App.scss';
+import Profile from './components/pages/Profile/Profile';
 
 function App() {
   return (
     <div className='App'>
       <Authentication />
+      <Profile />
     </div>
   );
 }
